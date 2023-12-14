6b249efe665677e30ae4a33d0dc91479f55fd908
HiddeWierper <hidde@wierper.net>
2023-12-14 17:52:32 +0100

    made level 4 and changed some links

---
+++ game/level4/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>InlogPage || CanYOuSee</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+<main>
+  <div class="title-screen">
+      <img id="eye" src="../../assets/eye.png"><h1>CAn YOu SeE?</h1>
+
+  </div>
+
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <div class="Inlog">
+      <p id="user">PaSsWoRd</p>
+      <label for="Password">
+      <input type="password" id="Password" name="password" placeholder="EnTeR PaSs!!"><br><br>
+      <input id="enter" type="submit" value="Enter">
+    </div>
+  </form>
+
+
+</main>
+
+<?php
+
+session_start();
+// if ($_SESSION["loggedin"] === true) {
+//     header("location: /");
+//     exit;
+// }
+
+try {
+//   when on server remove //
+  // $host = 'thuis.wierper.net';
+  // $pass = 'Wierper1411';
+
+
+  $pass = 'root';
+  $host = 'localhost';
+  $port='3306';
+  $user = 'root';
+  
+  $db= 'no-escape';
+  $dbh = new PDO('mysql: host=' . $host.'; dbname='.$db
+                .'; port=' . $port, $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Database Connection failed: " . $e->getMessage() . "<br><br>";
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (!empty($_POST["password"])) {
+        $password = $_POST["password"];
+
+        // Use prepared statements to prevent SQL injection
+        $sql = "SELECT * FROM loginpass WHERE password = :password AND id = 789";
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindParam(':password', $password);
+
+        try {
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            
+            if (count($result) > 0) {
+                $_SESSION["level4"] = true;
+                header("location: /no-escape");
+                exit;
+            } else {
+                echo "<script>alert('DuMb AsS!')
+                document.getElementById('enter').value = 'try again';
+                </script>";
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    } else {
+        echo "Please enter a username and password";
+    }
+}
+?>
+
+
+</body>
+
+</html>
