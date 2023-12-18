fb08206d7257f47e564e9af40a688cfc05665cea
marnix <marnixlorist@hotmail.com>
2023-12-18 11:02:22 +0100

    add basic endpage

---
+++ game/end/index.js
@@ -0,0 +1,10 @@
+
+
+function playAudio() {
+    var audio = document.getElementById("myAudio");
+    document.getElementById('overlay').style.display = "none";
+    audio.play().catch(function(error) {
+        console.log("Autoplay prevented: ", error);
+        
+    });
+}
